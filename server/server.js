fd23d48d366e24fcfaaf185ac523b4d6e14dbdd5
halimbinjohar <muhammadhalimbinjohar@gmail.com>
2025-10-19 21:35:52 +0800

    change post number

--- server/server.js
+++ server/server.js
@@ -1,11 +1,17 @@
 import http from "node:http"
 
-const PORT = 8000;
+const PORT = 5000;
 
 const server = http.createServer( (req, res) => {
 
+    const urlPath = req.url
+
+    console.log(urlPath)
+
     res.writeHead(200, {"Content-Type": "text/html"});
     res.end("<html><body><h1>Server.js: This server is running on port 8000 </h1></body></html>");
+
+
 });
 
 server.listen(PORT, () => {
